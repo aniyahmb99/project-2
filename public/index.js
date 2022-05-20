faa576f80c2984daec445ccb34c08fb3679ee00b
AJDewy <adam32thomas@gmail.com>
2022-05-19 20:08:05 -0400

    add Google Login script

--- public/index.js
+++ public/index.js
@@ -11,3 +11,10 @@ $(".dropdown .dropdown-menu li").click(function () {
   $(this).parents(".dropdown").find("span").text($(this).text());
   $(this).parents(".dropdown").find("input").attr("value", $(this).attr("id"));
 });
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
