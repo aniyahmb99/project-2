d2c4a9bb232f3d439ff3fd486b2028d539bbed2f
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-05-21 19:45:21 -0400

    sever side holiday selection and page generation template

--- overlay/overlay.js
+++ overlay/overlay.js
@@ -1,1 +1,72 @@
-const userInputTo = document.getElementById(inputTo).value;
+// Cerate an inquirer promp to select the holiday
+// Selected holiday will generate the select holiday card HTML 
+// When the user selects the card it will then move tp the preview 
+
+
+const inquirer = require("inquirer");
+const card = require("../overlay")
+
+function initApp() {
+
+    function selectHoliday() {
+        inquirer.prompt([
+            
+            {
+               type: "list",
+               name: "holiday",
+               message: "Select Holiday",
+               choices: [
+                    "Martin Luther King, Jr. Day",
+                    "Memorial Day",
+                    "Juneteenth",
+                    "Independence Day",
+                    "Labor Day",
+                    "Veterans Day",
+                    "Thanksgiving Day",
+                    "Christmas Day",
+                    "New Year's Eve",
+                    "Mother's Day",
+                    "Easter",
+                    "Halloween",
+                    "Father's Day",
+                    "Valentine's Day",
+                    "Saint Patrick's Day"
+               ] 
+            }
+        ]).then(choice => {
+            switch(choice.holiday) {
+                case "Martin Luther King, Jr. Day": generateMlk();
+                break;
+                case "Memorial Day": generateMemDay();
+                break;
+                case "Juneteenth": generateJune();
+                break;
+                case "Independence Day": generateIndDay();
+                break;
+                case "Labor Day<": generateLaborDay();
+                break;
+                case "Veterans Day": generateVetDay();
+                break;
+                case "Christmas Day": generateXmas();
+                break;
+                case "New Year's Eve": generateNYE();
+                break;
+                case "Mother's Day": generateMomDay();
+                break;
+                case "Easter": generateEaster();
+                break;
+                case "Halloween": generateHalloween();
+                break;
+                case "Father's Day": generateDadDay();
+                break;
+                case "Valentine's Day": generateValDay();
+                break;
+                case "Saint Patrick's Day": generateStPat();
+                break;
+            }
+        });
+    }
+    selectHoliday();
+
+}
+initApp();
