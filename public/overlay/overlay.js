e6ca2a85882c4266dd23eb36876ba7287025526d
bshort985 <97572888+bshort985@users.noreply.github.com>
2022-05-24 19:14:57 -0400

    final push

---
+++ public/overlay/overlay.js
@@ -0,0 +1,50 @@
+// Create an array with the holiday and respected cards
+// Add holidays to the dropdown menu
+// when use selects a holiday then the holiday cards appear
+// when user selects the card then it will move to the preview page
+
+
+
+function selectCard() {}
+
+
+function removeHide(){
+    document.getElementById.classList.remove("hide");
+}
+
+
+    function selectHoliday() {
+                switch(holiday) {
+                case "Martin Luther King, Jr. Day": removeHide();
+                break;
+                case "Memorial Day": generateMemDay();
+                break;
+                case "Juneteenth": generateJune();
+                break;
+                case "Independence Day": generateIndDay();
+                break;
+                case "Labor Day": generateLaborDay();
+                break;
+                case "Veterans Day": generateVetDay();
+                break;
+                case "Christmas Day": generateXmas();
+                break;
+                case "New Year's Eve": generateNYE();
+                break;
+                case "Mother's Day": generateMomDay();
+                break;
+                case "Easter": generateEaster();
+                break;
+                case "Halloween": generateHalloween();
+                break;
+                case "Father's Day": generateDadDay();
+                break;
+                case "Valentine's Day": generateValDay();
+                break;
+                case "Saint Patrick's Day": generateStPat();
+                break;
+                }
+            
+            };
+  
+
